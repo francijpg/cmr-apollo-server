58f851fa0f3cf79a32cb5a94cb19010ee9f057e5
francijpg <hellofrancijpg@gmail.com>
2020-11-04 08:12:24 -0300

    Update schema.js

--- db/schema.js
+++ db/schema.js
@@ -2,8 +2,28 @@ const { gql } = require("apollo-server");
 
 // Schema
 const typeDefs = gql`
+  type Usuario {
+    id: ID
+    nombre: String
+    apellido: String
+    email: String
+    creado: String
+  }
+  input UsuarioInput {
+    nombre: String!
+    apellido: String!
+    email: String!
+    password: String!
+  }
+
   type Query {
+    #Usuarios
     obtenerCurso: String
+  }
+
+  type Mutation {
+    # Usuarios
+    nuevoUsuario(input: UsuarioInput): Usuario
   }
 `;
 
