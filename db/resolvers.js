70abcbdaebfdc6a5ba6c44b3bd50431e245d3254
francijpg <hellofrancijpg@gmail.com>
2020-11-04 08:12:42 -0300

    Mutation => nuevoUsuario

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,6 +1,35 @@
+const Usuario = require("../models/Usuario");
+const bcryptjs = require("bcryptjs");
+
 const resolvers = {
   Query: {
     obtenerCurso: () => "cursos",
+  },
+  Mutation: {
+    nuevoUsuario: async (_, { input }) => {
+      const { email, password } = input;
+
+      // console.log(input)
+      // return "creando..."
+      // Revisar si el usuario ya esta registrado
+      const existeUsuario = await Usuario.findOne({ email });
+      if (existeUsuario) {
+        throw new Error("El usuario ya esta registrado");
+      }
+
+      // Hashear su password
+      const salt = await bcryptjs.genSalt(10);
+      input.password = await bcryptjs.hash(password, salt);
+
+      try {
+        // Guardarlo en la base de datos
+        const usuario = new Usuario(input);
+        usuario.save(); // guardarlo
+        return usuario;
+      } catch (error) {
+        console.log(error);
+      }
+    },
   },
 };
 
