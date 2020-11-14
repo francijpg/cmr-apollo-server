1ad22f51b3d1f146860b712c8e28075b25081ead
francijpg <hellofrancijpg@gmail.com>
2020-11-14 14:51:58 -0300

    create product's CRUD

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,4 +1,5 @@
 const Usuario = require("../models/Usuario");
+const Producto = require("../models/Producto");
 const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 require("dotenv").config({ path: "variables.env" });
@@ -13,8 +14,26 @@ const resolvers = {
   Query: {
     obtenerUsuario: async (_, { token }) => {
       const usuarioId = await jwt.verify(token, process.env.SECRET);
-      
+
       return usuarioId;
+    },
+    obtenerProductos: async () => {
+      try {
+        const productos = await Producto.find({});
+
+        return productos;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    obtenerProducto: async (_, { id }) => {
+      // revisar si el producto existe o no
+      const producto = await Producto.findById(id);
+      if (!producto) {
+        throw new Error("Producto no encontrado");
+      }
+
+      return producto;
     },
   },
   Mutation: {
@@ -62,6 +81,46 @@ const resolvers = {
       return {
         token: crearToken(existeUsuario, process.env.SECRET, "24h"),
       };
+    },
+    nuevoProducto: async (_, { input }) => {
+      try {
+        const producto = new Producto(input);
+
+        // almacenar en la bd
+        const resultado = await producto.save();
+
+        return resultado;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    actualizarProducto: async (_, { id, input }) => {
+      // revisar si el producto existe o no
+      let producto = await Producto.findById(id);
+
+      if (!producto) {
+        throw new Error("Producto no encontrado");
+      }
+
+      // guardarlo en la base de datos
+      producto = await Producto.findOneAndUpdate({ _id: id }, input, {
+        new: true,
+      });
+
+      return producto;
+    },
+    eliminarProducto: async (_, { id }) => {
+      // revisar si el producto existe o no
+      let producto = await Producto.findById(id);
+
+      if (!producto) {
+        throw new Error("Producto no encontrado");
+      }
+
+      // Eliminar
+      await Producto.findOneAndDelete({ _id: id });
+
+      return "Producto Eliminado";
     },
   },
 };
--- db/schema.js
+++ db/schema.js
@@ -14,11 +14,25 @@ const typeDefs = gql`
     token: String
   }
 
+  type Producto {
+    id: ID
+    nombre: String
+    existencia: Int
+    precio: Float
+    creado: String
+  }
+
   input UsuarioInput {
     nombre: String!
     apellido: String!
     email: String!
     password: String!
+  }
+
+  input ProductoInput {
+    nombre: String!
+    existencia: Int!
+    precio: Float!
   }
 
   input AutenticarInput {
@@ -27,14 +41,23 @@ const typeDefs = gql`
   }
 
   type Query {
-    #Usuarios
+    # Usuarios
     obtenerUsuario(token: String!): Usuario
+
+    # Productos
+    obtenerProductos: [Producto]
+    obtenerProducto(id: ID!): Producto
   }
 
   type Mutation {
     # Usuarios
     nuevoUsuario(input: UsuarioInput): Usuario
     autenticarUsuario(input: AutenticarInput): Token
+
+    # Productos
+    nuevoProducto(input: ProductoInput): Producto
+    actualizarProducto(id: ID!, input: ProductoInput): Producto
+    eliminarProducto(id: ID!): String
   }
 `;
 
