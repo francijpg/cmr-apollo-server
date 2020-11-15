f0ca2381527f06b1a0377dd2ff9b1cdb83bd7eb7
francijpg <hellofrancijpg@gmail.com>
2020-11-15 11:45:36 -0300

    add context

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const { ApolloServer, gql } = require("apollo-server");
 const typeDefs = require("./db/schema");
 const resolvers = require("./db/resolvers");
 const conectarDB = require("./config/db");
+const jwt = require("jsonwebtoken");
+require("dotenv").config({ path: "variables.env" });
 
 // Conectar a la base de datos
 conectarDB();
@@ -10,6 +12,23 @@ conectarDB();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: ({ req }) => {
+    const token = req.headers["authorization"] || "";
+    if (token) {
+      try {
+        const usuario = jwt.verify(
+          token, // token.replace("Bearer ", ""),
+          process.env.SECRET
+        );
+        return {
+          usuario,
+        };
+      } catch (error) {
+        console.log("Hubo un error");
+        console.log(error);
+      }
+    }
+  },
 });
 
 // arrancar el servidor
